aa91d536266b5485b31955aebc170129e7421ee0
christos1rekkas <rekkitos@yahoo.gr>
2021-07-12 07:42:16 +0300

    what I changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { UserProfileComponent } from './user-profile/user-profile.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  },
+  { path: 'user-profile', loadChildren: () => import('./user-profile/user-profile.module').then(m => m.UserProfileModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
+import { DateTimeComponent } from './date-time/date-time.component';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { BannerComponent } from './banner/banner.component';
+import { BoxComponent } from './box/box.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TableModule } from 'primeng/table';
+import { TableComponent } from './table/table.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { HighchartsChartModule } from 'highcharts-angular';
+import { HomeComponent } from './home/home.component';
+import { PaginatorModule } from 'primeng/paginator';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+      DateTimeComponent,
+      BoxComponent,
+      BannerComponent,
+      SideMenuComponent,
+      TableComponent,
+      PieChartComponent,
+      HomeComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    MenuModule,
+    PanelMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    HttpClientModule,
+    TableModule,
+    HighchartsChartModule,
+    PaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
